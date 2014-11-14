ff26578187117cc367c593fdc0c2759bfb220438
Larry O'Brien <lobrien@knowing.net>
2014-11-14 11:15:20 -1000

    javadoc-to-mdoc
    Ran javadoc-to-mdoc on Lollipop SDK. Then fixed up line endings. Still lots of wrapping issues. :-(
    
    Also, ack -l "sample external" | wc -l
         167
    
    Which doesn't seem right.

---
+++ docs/Mono.Android/en/convert.sh
@@ -0,0 +1,524 @@
+cd Android.AccessibilityServices
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Accounts
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Animation
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Annotation
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.App.Admin
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.App.Backup
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.App
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Appwidget
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Bluetooth
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Content.PM
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Content.Res
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Content
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Database.Sqlite
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Database
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Drm
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Gestures
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Graphics.Drawables.Shapes
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Graphics.Drawables
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Graphics.Pdf
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Graphics
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Hardware.Display
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Hardware.Input
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Hardware.Location
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Hardware.Usb
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Hardware
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.InputMethodServices
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Locations
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Media.Audiofx
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Media.Effect
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Media
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Mtp
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Net.Http
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Net.Nsd
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Net.Rtp
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Net.Sip
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Net.Wifi.P2p.Nsd
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Net.Wifi.P2p
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Net.Wifi
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Net
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Nfc.CardEmulators
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Nfc.Tech
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Nfc
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.OS.Storage
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.OS
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Opengl
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Preferences
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Print.Pdf
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Print
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.PrintServices
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Provider
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Renderscripts
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Runtime
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Sax
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Security
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Service.Dreams
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Service.Notification
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Service.Textservice
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Service.Wallpaper
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Speech.Tts
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Speech
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Telephony.Cdma
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Telephony.Gsm
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Telephony
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Test.Mock
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Test.Suitebuilder.Annotation
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Test.Suitebuilder
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Test
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Text.Format
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Text.Method
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Text.Style
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Text.Util
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Text
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Transitions
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Util
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Views.Accessibility
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Views.Animations
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Views.InputMethods
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Views.TextService
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Views
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Webkit
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android.Widget
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Android
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Dalvik.Annotation
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Dalvik.Bytecode
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Dalvik.SystemInterop
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Awt.Font
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Beans
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.IO
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Interop
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Lang.Annotation
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Lang.Ref
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Lang.Reflect
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Lang
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Math
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Net
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Nio.Channels.Spi
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Nio.Channels
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Nio.Charset.Spi
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Nio.Charset
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Nio
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Security.Acl
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Security.Cert
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Security.Interfaces
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Security.Spec
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Security
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Sql
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Text
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Util.Concurrent.Atomic
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Util.Concurrent.Locks
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Util.Concurrent
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Util.Jar
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Util.Logging
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Util.Prefs
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Util.Regex
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Util.Zip
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Java.Util
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Javax.Crypto.Interfaces
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Javax.Crypto.Spec
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Javax.Crypto
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Javax.Microedition.Khronos.Egl
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Javax.Microedition.Khronos.Opengles
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Javax.Net.Ssl
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Javax.Net
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Javax.Security.Auth.Callback
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Javax.Security.Auth.Login
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Javax.Security.Auth.X500
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Javax.Security.Auth
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Javax.Security.Cert
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Javax.Sql
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Javax.Xml.Datatype
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Javax.Xml.Namespace
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Javax.Xml.Parsers
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Javax.Xml.Transform.Dom
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Javax.Xml.Transform.Sax
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Javax.Xml.Transform.Stream
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Javax.Xml.Transform
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Javax.Xml.Validation
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Javax.Xml.Xpath
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Javax.Xml
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Commons.Logging
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Http.Auth.Params
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Http.Authentication
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Http.Client.Entity
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Http.Client.Methods
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Http.Client.Params
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Http.Client.Protocol
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Http.Client.Utils
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Http.Client
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Http.Conn.Params
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Http.Conn.Routing
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Http.Conn.Schemes
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Http.Conn.Ssl
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Http.Conn.Util
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Http.Conn
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Http.Cookie.Params
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Http.Cookies
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Http.Entity
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Http.IO
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Http.Message
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Http.Params
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Http.Protocol
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Http.Util
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Apache.Http
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Json
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.W3c.Dom.LS
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.W3c.Dom
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Xml.Sax.Ext
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Xml.Sax.Helpers
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Xml.Sax
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.XmlPull.V1
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd Org.Xmlpull.V1.Sax2
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd System.Drawing
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd System.IO
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
+cd System.Linq
+sed -i '' -e 's/'$'\r''$//' *.xml
+cd ..
